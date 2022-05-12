e9b12f6035f42882a35ce909752740de6cc2c699
Mathias Bretonneau <Mathias.bretonneau@gmail.com>
2022-05-12 17:33:09 +0200

    Add API

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,27 @@
 
 namespace App\Controller;
 
+use App\Model\AirManager;
+
 class HomeController extends AbstractController
 {
-    /**
-     * Display home page
-     */
+    public array $airQuality = [
+        1 => 'Très bon',
+        2 => 'Bon',
+        3 => 'Bof',
+        4 => 'Faible',
+        5 => 'JS',
+    ];
+
     public function index(): string
     {
-        return $this->twig->render('Home/index.html.twig');
+        $airManager = new AirManager();
+        $air = $airManager->show();
+        $detailsAirQuality = $air ['list'][0]['main']['aqi'];
+
+        return $this->twig->render('Home/index.html.twig', [
+            'airQuality' => $this->airQuality,
+            'detailsAirQuality' => $detailsAirQuality,
+        ]);
     }
 }
