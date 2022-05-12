c4273628e27f3a436feaaa25cd4ee8092491bf9b
Daniel ESCARAVAGE <escaravage.daniel@gmail.com>
2022-05-12 21:50:42 +0200

    ajout d'un redirection après connexion

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -20,12 +20,12 @@ class LoginController extends AbstractController
                 if ($user) {
                     if (password_verify($connection['password'], $user['password'])) {
                         $_SESSION['user'] = $user['id'];
-                        header('Location: /admin/');
+                        header('Location: /');
                     } else {
                         $errors[] = 'Mot de passe inconnu';
                     }
                 } else {
-                    $errors[] = 'Name inconnu';
+                    $errors[] = 'Le nom est inconnu';
                 }
             }
         }
