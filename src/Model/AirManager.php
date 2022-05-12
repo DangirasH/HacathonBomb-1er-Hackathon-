948b03e7c04f071927f276a2632fdc654f33425f
Mathias Bretonneau <Mathias.bretonneau@gmail.com>
2022-05-12 17:38:03 +0200

    Add API

--- src/Model/AirManager.php
+++ src/Model/AirManager.php
@@ -11,8 +11,14 @@ class AirManager extends AbstractManager
         $client = HttpClient::create();
         $response = $client->request(
             'GET',
-            'http://
-            api.openweathermap.org/data/2.5/air_pollution?lat=50.06&lon=01.49&appid=1c368d32a1cabbfeb39f4f2186294792'
+            'http://api.openweathermap.org/data/2.5/air_pollution',
+            [
+                'query' => [
+                    'lat' => '50.06',
+                    'lon' => '01.49',
+                    'appid' => '1c368d32a1cabbfeb39f4f2186294792'
+                ],
+            ]
         );
 
         $statusCode = $response->getStatusCode();
